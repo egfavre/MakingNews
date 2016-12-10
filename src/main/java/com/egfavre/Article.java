5cc5dc987d467a327a17bace18cf652dd34f4133
Liz Favre <egfavre@gmail.com>
2016-12-09 20:29:49 -0500

    Initial Commit

---
+++ src/main/java/com/egfavre/Article.java
@@ -0,0 +1,14 @@
+package com.egfavre;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by user on 12/9/16.
+ */
+@Entity
+public class Article {
+    String text;
+
+    public Article() {
+    }
+}
--- src/main/java/com/egfavre/Controller.java
+++ src/main/java/com/egfavre/Controller.java
@@ -1,10 +1,13 @@
 package com.egfavre;
 
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
 
@@ -14,6 +17,7 @@ import java.sql.SQLException;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
     @PostConstruct
     public void init() throws SQLException {
         Server.createWebServer().start();
@@ -22,6 +26,12 @@ public class Controller {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String welcome () {
         return "welcome";
+    }
+
+    @RequestMapping(path = "/scrape", method = RequestMethod.POST)
+    public String scrape() {
+        Article article = new Article();
+        return "redirect:/edit";
     }
 
 }
