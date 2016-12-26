2c35257d2750e3fe4c14ce5b3b74aba76220d987
Liz Favre <egfavre@gmail.com>
2016-12-26 17:55:15 -0500

    can count POS's but I think I want to change this so each POS generates an array list so I can count and replace nth words.

--- src/main/java/com/egfavre/Controller.java
+++ src/main/java/com/egfavre/Controller.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 public class Controller {
 
     MaxentTagger tagger = new MaxentTagger("/Users/user/IdeaProjects/MakingNews/models/english-bidirectional-distsim.tagger");
-
+    String tagged;
 
     @PostConstruct
     public void init() throws SQLException {
@@ -48,6 +48,11 @@ public class Controller {
     public String scrape() throws Exception{
         //get document
         Document doc = Jsoup.connect("http://www.tmz.com").get();
+        Element body = doc.body();
+        String bodyString = (body.text()).toString();
+        tagged = tagger.tagString(bodyString);
+
+        /*
         Elements paragraphs = doc.select("p");
 
         //convert element text to string
@@ -55,16 +60,85 @@ public class Controller {
         for(Element p : paragraphs) {
             String nextParaString = (p.text()).toString();
             paraStrings.add(nextParaString);
-        }
+        }*/
 
-        //add POS tagging
-        ArrayList<String> taggedParaStrings = new ArrayList<>();
-        for (String ps:paraStrings) {
-            String tagged = tagger.tagString(ps);
-            taggedParaStrings.add(tagged);
-            System.out.println(tagged);
-        }
+
+        //count tagged POS's
+        /*
+        JJ Adjective
+        NN Noun, singular or mass
+        NNS Noun, plural
+        RB Adverb
+        UH Interjection
+        VB Verb, base form
+        VBD Verb, past tense
+       */
+        countNouns(tagged);
+        countPluralNouns(tagged);
+        countAdjectives(tagged);
+
+
+
+
+
+
 
         return "redirect:/edit";
     }
+
+    public static void countNouns(String tagged) {
+        String findStr = "_NN ";
+        int lastIndex = 0;
+        int count = 0;
+
+        while(lastIndex != -1){
+
+            lastIndex = tagged.indexOf(findStr,lastIndex);
+
+            if(lastIndex != -1){
+                count ++;
+                lastIndex += findStr.length();
+            }
+        }
+        System.out.println("This stream contains " + count + " nouns.");
+
+    }
+
+    public static void countPluralNouns(String tagged) {
+        String findStr = "_NNS ";
+        int lastIndex = 0;
+        int count = 0;
+
+        while(lastIndex != -1){
+
+            lastIndex = tagged.indexOf(findStr,lastIndex);
+
+            if(lastIndex != -1){
+                count ++;
+                lastIndex += findStr.length();
+            }
+        }
+        System.out.println("This stream contains " + count + " plural nouns.");
+
+    }
+
+    public static void countAdjectives(String tagged) {
+        String findStr = "_JJ ";
+        int lastIndex = 0;
+        int count = 0;
+
+        while(lastIndex != -1){
+
+            lastIndex = tagged.indexOf(findStr,lastIndex);
+
+            if(lastIndex != -1){
+                count ++;
+                lastIndex += findStr.length();
+            }
+        }
+        System.out.println("This stream contains " + count + " adjectives.");
+
+    }
+
+
 }
