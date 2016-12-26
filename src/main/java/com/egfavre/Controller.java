87762e86b501ac7741614c447bb7a5f703bb40ea
Liz Favre <egfavre@gmail.com>
2016-12-26 15:14:15 -0500

    Broke JSOUP imports- deleted Jsoup and boiler plate and reloading

--- src/main/java/com/egfavre/Controller.java
+++ src/main/java/com/egfavre/Controller.java
@@ -1,31 +1,14 @@
 package com.egfavre;
 
-import de.l3s.boilerpipe.BoilerpipeExtractor;
-import de.l3s.boilerpipe.extractors.CommonExtractors;
-import de.l3s.boilerpipe.sax.HtmlArticleExtractor;
-import javafx.scene.web.WebView;
+
 import org.h2.tools.Server;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpSession;
-import javax.swing.text.Document;
-import javax.swing.text.html.HTMLDocument;
-import javax.swing.text.html.HTMLEditorKit;
-import javax.swing.text.html.parser.ParserDelegator;
-import java.io.Reader;
-import java.io.StringReader;
-import java.net.URL;
 import java.sql.SQLException;
-import java.util.ArrayList;
-
-import static javafx.scene.input.KeyCode.R;
 
 
 /**
@@ -35,9 +18,6 @@ import static javafx.scene.input.KeyCode.R;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    org.jsoup.nodes.Document doc;
-    final BoilerpipeExtractor extractor = CommonExtractors.ARTICLE_EXTRACTOR;
-    final HtmlArticleExtractor htmlExtr = HtmlArticleExtractor.INSTANCE;
 
     @PostConstruct
     public void init() throws SQLException {
@@ -50,27 +30,14 @@ public class Controller {
     }
 
     @RequestMapping(path = "/edit", method = RequestMethod.GET)
-    public String edit (Model model) throws Exception{
-        model.addAttribute("doc", doc);
+    public String edit () throws Exception{
+
 
         return "edit";
     }
 
     @RequestMapping(path = "/scrape", method = RequestMethod.POST)
     public String scrape() throws Exception{
-        URL url = new URL(
-                "http://www.tmz.com");
-        String text = htmlExtr.process(extractor, url);
-
-        System.out.println(text);
-
-        Reader stringReader = new StringReader(text);
-        HTMLEditorKit htmlKit = new HTMLEditorKit();
-        HTMLDocument htmlDoc = (HTMLDocument) htmlKit.createDefaultDocument();
-        HTMLEditorKit.Parser parser = new ParserDelegator();
-        parser.parse(stringReader, htmlDoc.getReader(0), true);
-
-        System.out.println(parser.toString());
 
 
         return "redirect:/edit";
