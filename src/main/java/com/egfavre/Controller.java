32b7f0596b7b983721c3862d66c390683de4f66c
Liz Favre <egfavre@gmail.com>
2016-12-26 18:32:44 -0500

    making POS lists and counting

--- src/main/java/com/egfavre/Controller.java
+++ src/main/java/com/egfavre/Controller.java
@@ -33,38 +33,91 @@ public class Controller {
     }
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String welcome () {
+    public String welcome() {
         return "welcome";
     }
 
     @RequestMapping(path = "/edit", method = RequestMethod.GET)
-    public String edit () throws Exception{
-
-
+    public String edit() throws Exception {
         return "edit";
     }
 
     @RequestMapping(path = "/scrape", method = RequestMethod.POST)
-    public String scrape() throws Exception{
+    public String scrape() throws Exception {
+
         //get document
         Document doc = Jsoup.connect("http://www.tmz.com").get();
+
+        //tag text String
         Element body = doc.body();
         String bodyString = (body.text()).toString();
         tagged = tagger.tagString(bodyString);
 
-        /*
-        Elements paragraphs = doc.select("p");
+        //put tagged words into a list
+        String[] allTaggedWords = tagged.split("\\s+");
 
-        //convert element text to string
-        ArrayList<String> paraStrings = new ArrayList<>();
-        for(Element p : paragraphs) {
-            String nextParaString = (p.text()).toString();
-            paraStrings.add(nextParaString);
-        }*/
+        //separate words into separate lists based on POS
+        ArrayList<String> nouns = new ArrayList<>();
+        for (String noun : allTaggedWords) {
+            if ((noun.endsWith("_NN"))) {
+                nouns.add(noun);
+            }
+        }
+        System.out.println("there are " + nouns.size() + " nouns");
 
+        ArrayList<String> pluralNouns = new ArrayList<>();
+        for (String pns : allTaggedWords) {
+            if ((pns.endsWith("_NNS"))) {
+                pluralNouns.add(pns);
+            }
+        }
+        System.out.println("there are " + pluralNouns.size() + " plural nouns");
 
-        //count tagged POS's
-        /*
+        ArrayList<String> adjectives = new ArrayList<>();
+        for (String adj : allTaggedWords) {
+            if ((adj.endsWith("_JJ"))) {
+                adjectives.add(adj);
+            }
+        }
+        System.out.println("there are " + adjectives.size() + " adjectives");
+
+        ArrayList<String> adverbs = new ArrayList<>();
+        for (String adv : allTaggedWords) {
+            if ((adv.endsWith("_RB"))) {
+                adverbs.add(adv);
+            }
+        }
+        System.out.println("there are " + adverbs.size() + " adverbs");
+
+        ArrayList<String> interjections = new ArrayList<>();
+        for (String interj : allTaggedWords) {
+            if ((interj.endsWith("_UH"))) {
+                interjections.add(interj);
+            }
+        }
+        System.out.println("there are " + interjections.size() + " interjections");
+
+        ArrayList<String> verbs = new ArrayList<>();
+        for (String verb : allTaggedWords) {
+            if ((verb.endsWith("_VB"))) {
+                verbs.add(verb);
+            }
+        }
+        System.out.println("there are " + verbs.size() + " verbs");
+
+        ArrayList<String> pastTenseVerbs = new ArrayList<>();
+        for (String ptv : allTaggedWords) {
+            if ((ptv.endsWith("_VBD"))) {
+                pastTenseVerbs.add(ptv);
+            }
+        }
+        System.out.println("there are " + pastTenseVerbs.size() + " past tense verbs");
+
+        return "redirect:/edit";
+    }
+}
+
+    /*
         JJ Adjective
         NN Noun, singular or mass
         NNS Noun, plural
@@ -73,72 +126,13 @@ public class Controller {
         VB Verb, base form
         VBD Verb, past tense
        */
-        countNouns(tagged);
-        countPluralNouns(tagged);
-        countAdjectives(tagged);
 
+ /*
+        Elements paragraphs = doc.select("p");
 
-
-
-
-
-
-        return "redirect:/edit";
-    }
-
-    public static void countNouns(String tagged) {
-        String findStr = "_NN ";
-        int lastIndex = 0;
-        int count = 0;
-
-        while(lastIndex != -1){
-
-            lastIndex = tagged.indexOf(findStr,lastIndex);
-
-            if(lastIndex != -1){
-                count ++;
-                lastIndex += findStr.length();
-            }
-        }
-        System.out.println("This stream contains " + count + " nouns.");
-
-    }
-
-    public static void countPluralNouns(String tagged) {
-        String findStr = "_NNS ";
-        int lastIndex = 0;
-        int count = 0;
-
-        while(lastIndex != -1){
-
-            lastIndex = tagged.indexOf(findStr,lastIndex);
-
-            if(lastIndex != -1){
-                count ++;
-                lastIndex += findStr.length();
-            }
-        }
-        System.out.println("This stream contains " + count + " plural nouns.");
-
-    }
-
-    public static void countAdjectives(String tagged) {
-        String findStr = "_JJ ";
-        int lastIndex = 0;
-        int count = 0;
-
-        while(lastIndex != -1){
-
-            lastIndex = tagged.indexOf(findStr,lastIndex);
-
-            if(lastIndex != -1){
-                count ++;
-                lastIndex += findStr.length();
-            }
-        }
-        System.out.println("This stream contains " + count + " adjectives.");
-
-    }
-
-
-}
+        //convert element text to string
+        ArrayList<String> paraStrings = new ArrayList<>();
+        for(Element p : paragraphs) {
+            String nextParaString = (p.text()).toString();
+            paraStrings.add(nextParaString);
+        }*/
